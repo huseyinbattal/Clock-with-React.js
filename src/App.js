3c00e0b6f6ec57bdd095ad496c234abac2253725
huseyinbatta <huseyinbattal42@gmail.com>
2022-06-10 18:03:26 +0300

    clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
+import GrowExample from "./Snipper";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+function FormattedDate(props) {
+  return <h2>{props.date.toLocaleTimeString()}</h2>;
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Clock</h1>
+        <FormattedDate date={this.state.date} />
+        <h3><GrowExample/></h3>
+      </div>
+    );
+  }
+}
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Clock />
     </div>
   );
 }
-
-export default App;
---
+++ src/Snipper.js
@@ -0,0 +1,7 @@
+import Spinner from 'react-bootstrap/Spinner';
+
+function GrowExample() {
+  return <Spinner animation="grow" variant='danger'/>;
+}
+
+export default GrowExample;
